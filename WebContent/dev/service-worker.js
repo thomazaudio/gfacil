46febce67198e4fc2ec9efae5ae68dc312cbdcbd
Thomaz <thomaz-guitar@hotmail.com>
2017-09-05 06:40:01 -0300

    alterações no service-worker

--- WebContent/dev/service-worker.js
+++ WebContent/dev/service-worker.js
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var dataCacheName = 'Data-v1';
-var cacheName = 'PWA-final-1';
+var dataCacheName = 'CeasaPlusV1';
+var cacheName = 'CeasaPlusV1';
 var filesToCache = [
    '/',
   'css/style.css',

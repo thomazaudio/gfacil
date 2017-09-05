d08e27f3a9b272e7183183edddcf81a66af3d5fb
Thomaz <thomaz-guitar@hotmail.com>
2017-09-05 08:32:33 -0300

    outros

--- WebContent/dev/service-worker.js
+++ WebContent/dev/service-worker.js
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 var dataCacheName = 'CeasaPlusV1';
 var cacheName = 'CeasaPlusV1';
 var filesToCache = [

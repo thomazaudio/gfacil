90428c178fd933e07749a1ebb12ec8ead7ebd382
Thomaz <thomaz-guitar@hotmail.com>
2017-09-07 17:49:34 -0300

    --

--- WebContent/dev/service-worker.js
+++ WebContent/dev/service-worker.js
@@ -11,8 +11,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var dataCacheName = 'CeasaPlus2.3';
-var cacheName = 'CeasaPlus2.3';
+var dataCacheName = 'CeasaPlus2.90';
+var cacheName = 'CeasaPlus2.90';
 var filesToCache = [
   'css/style.css',
   'global/lib/spin.js',
